4332029195cb4e0babb13d3378cbdfa48de83d29
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-11-17 18:36:08 +1100

    chore: create the sidebar for dashboard and the include the required UI components

--- apps/myorganizer/src/app/page.tsx
+++ apps/myorganizer/src/app/page.tsx
@@ -1,19 +1,39 @@
-import { Button } from "@myorganizer/web-ui";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator, Button, Separator, SidebarInset, SidebarProvider, SidebarTrigger } from "@myorganizer/web-ui";
+import { AppSidebar } from "../components/app-sidebar";
 
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.tailwind file.
-   */
   return (
-    <div className="flex items-center gap-3 p-2">
-      <Button variant={"default"}>Default</Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"link"}>Link</Button>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
--- libs/web-ui/src/index.ts
+++ libs/web-ui/src/index.ts
@@ -1,1 +1,14 @@
 export * from './lib/components/Button/Button';
+export * from './lib/components/Card/Card';
+export * from './lib/components/DropdownMenu/DropdownMenu';
+export * from './lib/components/Tooltip/Tooltip';
+export * from './lib/components/Skeleton/Skeleton';
+export * from './lib/components/Sidebar/Sidebar';
+export * from './lib/components/Label/Label';
+export * from './lib/components/Separator/Separator';
+export * from './lib/components/Breadcrumb/Breadcrumb';
+export * from './lib/components/Avatar/Avatar';
+export * from './lib/components/Collapsible/Collapsible';
+
+export * from './lib/hooks/use-media-query';
+export * from './lib/hooks/use-mobile';
