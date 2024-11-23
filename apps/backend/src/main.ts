d3db38310f63f03ebd7a14bc62eb734ab4be448c
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2024-11-23 22:01:58 +1100

    chore: include cors option for backend API

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,17 +4,24 @@
  */
 
 import express from 'express';
+import cors from 'cors';
 import * as path from 'path';
 
 const app = express();
 
+// CORS is enabled for the selected origins
+let corsOptions = {
+  origin: [ 'https://myorganizerapi.mnfprofile.com', 'http://localhost:3000' ]
+};
+
+app.use(cors(corsOptions));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to backend!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
